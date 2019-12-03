488cdc872ff57817bc755ac308a7c01efcf3e4a6
lucy <lucy@luz.lu>
2019-12-03 14:24:53 +0100

    a

--- 03.hs
+++ 03.hs
@@ -1,19 +1,16 @@
-{-# LANGUAGE TupleSections #-}
 import qualified Data.Map.Strict as M
-z (x, y) n 'U' = map (x, ) [y, y - 1 .. y - n]
-z (x, y) n 'D' = map (x, ) [y .. y + n]
-z (x, y) n 'L' = map (, y) [x, x - 1 .. x - n]
-z (x, y) n 'R' = map (, y) [x .. x + n]
-f (p, o, s) (c, n) | t <- z p n c =
-  (last t, o + n, M.union (M.fromList $ zip t [o ..]) s)
-g l | (_, _, m) <- foldl f ((0, 0), 0, M.empty) l = m
-d (x, y) = abs x + abs y
-k a b = (f M.keys, f M.elems)
- where
-  f g = minimum $ filter (/= 0) $ g $ M.mapKeys d $ M.intersectionWith (+) a b
+data P = P Int Int deriving (Eq, Ord)
+z (P x y) n 'U' = map (x `P`) [y, y - 1 .. y - n]
+z (P x y) n 'D' = map (x `P`) [y .. y + n]
+z (P x y) n 'L' = map (`P` y) [x, x - 1 .. x - n]
+z (P x y) n 'R' = map (`P` y) [x .. x + n]
+f (p, o, s) (c, n) | t <- z p n c = (last t, o + n, zip t [o ..] ++ s)
+g l | (_, _, m) <- foldl f (P 0 0, 0, []) l = M.delete (P 0 0) $ M.fromList m
+d (P x y) = abs x + abs y
+k m = (f M.keys, f M.elems) where f g = minimum $ g $ M.mapKeys d $ m
 r ',' = ' '
 r c   = c
 u (c : n) = (c, read n)
-p = map u . words . map r
-s s | (a, _ : b) <- break (== '\n') s = k (g $ p a) (g . p $ init b)
+p = g . map u . words . map r
+s s | (a, b) <- break (== '\n') s = k $ M.intersectionWith (+) (p a) (p b)
 main = interact $ show . s
