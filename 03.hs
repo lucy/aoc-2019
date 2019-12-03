6032241a410092b775868fdd6a6bafbab03eaced
lucy <lucy@luz.lu>
2019-12-03 14:31:04 +0100

    b

--- 03.hs
+++ 03.hs
@@ -1,16 +1,14 @@
+{-# LANGUAGE TupleSections, LambdaCase #-}
 import qualified Data.Map.Strict as M
-data P = P Int Int deriving (Eq, Ord)
-z (P x y) n 'U' = map (x `P`) [y, y - 1 .. y - n]
-z (P x y) n 'D' = map (x `P`) [y .. y + n]
-z (P x y) n 'L' = map (`P` y) [x, x - 1 .. x - n]
-z (P x y) n 'R' = map (`P` y) [x .. x + n]
+z (x, y) n 'U' = map (x, ) [y, y - 1 .. y - n]
+z (x, y) n 'D' = map (x, ) [y .. y + n]
+z (x, y) n 'L' = map (, y) [x, x - 1 .. x - n]
+z (x, y) n 'R' = map (, y) [x .. x + n]
 f (p, o, s) (c, n) | t <- z p n c = (last t, o + n, zip t [o ..] ++ s)
-g l | (_, _, m) <- foldl f (P 0 0, 0, []) l = M.delete (P 0 0) $ M.fromList m
-d (P x y) = abs x + abs y
-k m = (f M.keys, f M.elems) where f g = minimum $ g $ M.mapKeys d $ m
-r ',' = ' '
-r c   = c
+g l | (_, _, m) <- foldl f ((0, 0), 0, []) l = M.delete (0, 0) $ M.fromList m
+d (x, y) = abs x + abs y
+k m = (f M.keys, f M.elems) where f = minimum . ($ M.mapKeys d m)
 u (c : n) = (c, read n)
-p = g . map u . words . map r
+p = g . map u . words . map (\case ',' -> ' '; c -> c)
 s s | (a, b) <- break (== '\n') s = k $ M.intersectionWith (+) (p a) (p b)
 main = interact $ show . s
