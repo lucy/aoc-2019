d0b07e6a0e909c3da318ba9f5f636c98297bc093
lucy <lucy@luz.lu>
2019-12-04 06:39:05 +0100

    fix

--- 04.hs
+++ 04.hs
@@ -1,5 +1,5 @@
 import Data.List
-f t s = any t (map length (group s)) && length s == 6 && sort s == s
-g r = (h (>= 2), h (== 2)) where h t = length . filter (f t . show) r
+f t s = any t (map length (group s)) && sort s == s
+g r = (h (>= 2), h (== 2)) where h t = length $ filter (f t . show) r
 p s | (x, (_ : y)) <- break (== '-') s = [read x :: Int .. read y]
 main = interact $ show . g . p
