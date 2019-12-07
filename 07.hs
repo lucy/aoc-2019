c0aaea20c7947b2a998130d17e02a2fdc0bffa57
lucy <lucy@luz.lu>
2019-12-07 19:36:58 +0100

    meow

--- 07.hs
+++ 07.hs
@@ -6,16 +6,16 @@ import Data.List
 vm :: Array Int Int -> [Int] -> [Int]
 vm a input = runST $ do
   m <- thaw a :: ST s (STArray s Int Int)
-  let go input pc = do
+  let
+    go input pc = do
       ins <- readArray m pc
       let
         read i 1 = readArray m (pc + i)
-        read i 0 = readArray m (pc + i) >>= readArray m
-        write i x = readArray m (pc + i) >>= \i' -> writeArray m i' x
-        op = ins `mod` 100
-        i1 = read 1 ((ins `quot` 100) `mod` 10)
-        i2 = read 2 ((ins `quot` 1000) `mod` 10)
-      case op of
+        read i 0 = read i 1 >>= readArray m
+        write i x = read i 1 >>= \i' -> writeArray m i' x
+        i1 = read 1 (ins `quot` 100 `mod` 10)
+        i2 = read 2 (ins `quot` 1000 `mod` 10)
+      case ins `mod` 100 of
         1 -> do x <- i1; y <- i2; write 3 (x + y); go input (pc + 4)
         2 -> do x <- i1; y <- i2; write 3 (x * y); go input (pc + 4)
         3 -> do write 1 (head input); go (tail input) (pc + 2)
@@ -25,7 +25,6 @@ vm a input = runST $ do
         7 -> do x <- i1; y <- i2; write 3 (if x < y then 1 else 0); go input (pc + 4)
         8 -> do x <- i1; y <- i2; write 3 (if x == y then 1 else 0); go input (pc + 4)
         99 -> return []
-        _  -> error $ "invalid opcode: " ++ show ins ++ " " ++ show pc
   go input 0
 amp :: Array Int Int -> [Int] -> Int
 amp m (x : xs) = last e
@@ -34,11 +33,10 @@ amp m (x : xs) = last e
   e = foldr f a xs
   a = vm m (x : 0 : e)
 f :: Array Int Int -> [Int] -> Int
-f m p = maximum $ map (amp m) (permutations p)
+f m p = maximum (map (amp m) (permutations p))
 parse :: String -> Array Int Int
-parse s = let l = read $ "[" ++ s ++ "]" in listArray (0, length l) l
+parse s = let l = read ("[" ++ s ++ "]") in listArray (0, length l) l
 main :: IO ()
 main = do
   m <- parse <$> getContents
-  print $ f m [0 .. 4]
-  print $ f m [5 .. 9]
+  print (f m [0 .. 4], f m [5 .. 9])
