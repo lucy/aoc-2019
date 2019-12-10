143bfd190b19552ce083b3d6ee8e093381d73cf5
lucy <lucy@luz.lu>
2019-12-10 20:10:51 +0100

    stupid

--- 09.hs
+++ 09.hs
@@ -1,36 +1,26 @@
-{-# LANGUAGE FlexibleContexts #-}
-import qualified Data.Map.Strict as M
 import Data.Int
-type Mem = M.Map Int64 Int64
-vm :: Mem -> [Int64] -> [Int64]
-vm m ch =
-  let
-    go m ch rb pc =
-      let
-        r i = M.findWithDefault 0 i m
-        w i x = M.insert i x m
-        ix i 1 = pc + i
-        ix i 0 = r (pc + i)
-        ix i 2 = r (pc + i) + rb
-        is = r pc
-        p1 = ix 1 (is `div` 100 `mod` 10)
-        p2 = ix 2 (is `div` 1000 `mod` 10)
-        p3 = ix 3 (is `div` 10000 `mod` 10)
-      in case is `mod` 100 of
-        1 -> go (w p3 $ r p1 + r p2) ch rb (pc + 4)
-        2 -> go (w p3 $ r p1 * r p2) ch rb (pc + 4)
-        3 -> case ch of (x : xs) -> go (w p1 x) xs rb (pc + 2)
-        4 -> r p1 : go m ch rb (pc + 2)
-        5 -> go m ch rb (if r p1 /= 0 then r p2 else pc + 3)
-        6 -> go m ch rb (if r p1 == 0 then r p2 else pc + 3)
-        7 -> go (w p3 $ if r p1 < r p2 then 1 else 0) ch rb (pc + 4)
-        8 -> go (w p3 $ if r p1 == r p2 then 1 else 0) ch rb (pc + 4)
-        9 -> go m ch (rb + r p1) (pc + 2)
-        99 -> []
-  in go m ch 0 0
-parse :: String -> Mem
-parse s = let l = read ("[" ++ s ++ "]") in M.fromList (zip [0 ..] l)
-main :: IO ()
+import qualified Data.Map.Strict as M
+vm :: M.Map Int64 Int64 -> [Int64] -> Int64 -> Int64 -> [Int64]
+vm m ch rb pc = case is `mod` 100 of
+  1 -> vm (w p3 $ r p1 + r p2) ch rb (pc + 4)
+  2 -> vm (w p3 $ r p1 * r p2) ch rb (pc + 4)
+  3 -> vm (w p1 (head ch)) (tail ch) rb (pc + 2)
+  4 -> r p1 : vm m ch rb (pc + 2)
+  5 -> vm m ch rb (if r p1 /= 0 then r p2 else pc + 3)
+  6 -> vm m ch rb (if r p1 == 0 then r p2 else pc + 3)
+  7 -> vm (w p3 $ if r p1 < r p2 then 1 else 0) ch rb (pc + 4)
+  8 -> vm (w p3 $ if r p1 == r p2 then 1 else 0) ch rb (pc + 4)
+  9 -> vm m ch (rb + r p1) (pc + 2)
+  99 -> []
+ where
+  r i = M.findWithDefault 0 i m
+  w i x = M.insert i x m
+  is = r pc
+  ix i p = case is `quot` p `mod` 10 of
+    1 -> pc + i
+    0 -> r (pc + i)
+    2 -> r (pc + i) + rb
+  (p1, p2, p3) = (ix 1 100, ix 2 1000, ix 3 10000)
 main = do
-  m <- parse <$> getContents
-  print (last $ vm m [1], last $ vm m [2])
+  m <- M.fromList . zip [0..] . read . ('[':) . (++"]") <$> getContents
+  print (last $ vm m [1] 0 0, last $ vm m [2] 0 0)
