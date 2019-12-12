2e8045d9580f06e1299f1b5f1570e646e779fd00
lucy <lucy@luz.lu>
2019-12-12 13:17:34 +0100

    aaaaaa

--- 12.hs
+++ 12.hs
@@ -1,6 +1,6 @@
 import Data.List
+import Data.Maybe
 import Text.ParserCombinators.ReadP
-import qualified Data.Set as S
 
 parse :: String -> [[Int]]
 parse s = fst (last (readP_to_S (many p) s))
@@ -9,22 +9,15 @@ parse s = fst (last (readP_to_S (many p) s))
   p = skipSpaces *> char '<' *> mapM v ['x', 'y', 'z'] <* char '>'
 
 sim :: [(Int, Int)] -> [(Int, Int)]
-sim l =
-  [ (a + v', v')
-  | (a, v) <- l
-  , let v' = v + sum [ signum (b - a) | (b, _) <- l ]
-  ]
+sim l = [ (a + v', v') | (a, v) <- l
+        , let v' = v + sum [ signum (b - a) | (b, _) <- l ] ]
 
 energy :: [(Int, Int)] -> Int
 energy l = sum (map abs c) * sum (map abs v) where (c, v) = unzip l
-
-findCycle :: (Ord a) => S.Set a -> [a] -> Int
-findCycle s (x : xs)
-  | S.member x s = S.size s
-  | otherwise    = findCycle (S.insert x s) xs
 
 main :: IO ()
 main = do
   u <- map (map (\x -> (x, 0))) . transpose . parse <$> getContents
   print $ sum (map energy (transpose (iterate (map sim) u !! 1000)))
-  print $ foldl1 lcm (map (findCycle S.empty) (map (iterate sim) u))
+  print $ foldl1 lcm $ map succ $ catMaybes $
+    zipWith elemIndex u (tail . iterate sim <$> u)
