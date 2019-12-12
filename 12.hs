927225068d57be0ca61ed40103303991f1cf8434
lucy <lucy@luz.lu>
2019-12-12 11:18:20 +0100

    stuff

--- 12.hs
+++ 12.hs
@@ -1,42 +1,41 @@
+{-# LANGUAGE TupleSections #-}
 import Text.ParserCombinators.ReadP
 import qualified Data.Set as S
+import Data.List
 
-data V3 = V3 { _x, _y, _z :: Int } deriving (Eq, Ord)
+type U = [[(Int, Int)]]
 
-parse :: String -> [V3]
+parse :: String -> [(Int, Int, Int)]
 parse s = fst $ last $ readP_to_S (many p) s
  where
   v c = char c *> char '=' *> readS_to_P reads <* optional (string ", ")
-  p = V3 <$ skipSpaces <* char '<' <*> v 'x' <*> v 'y' <*> v 'z' <* char '>'
+  p = (,,) <$ skipSpaces <* char '<' <*> v 'x' <*> v 'y' <*> v 'z' <* char '>'
 
-sim :: [(V3, V3)] -> [(V3, V3)]
+sim :: [(Int, Int)] -> [(Int, Int)]
 sim l = map (move . foldl1 velocity) $ rotations
-  where rotations = take (length l) $ iterate (\l -> (last l) : (init l)) l
+  where rotations = take (length l) $ iterate (\l -> (tail l) ++ [head l]) l
 
-velocity :: (V3, V3) -> (V3, V3) -> (V3, V3)
-velocity (a@(V3 x y z), V3 xv yv zv) (V3 u v h, _) =
-  (a, V3 (vel x u xv) (vel y v yv) (vel z h zv))
- where
-  vel a b v
-    | a < b     = v + 1
-    | a > b     = v - 1
-    | otherwise = v
+velocity :: (Int, Int) -> (Int, Int) -> (Int, Int)
+velocity (a, v) (b, _)
+  | a < b     = (a, v + 1)
+  | a > b     = (a, v - 1)
+  | otherwise = (a, v)
 
-move :: (V3, V3) -> (V3, V3)
-move (V3 x y z, V3 xv yv zv) = (V3 (x + xv) (y + yv) (z + zv), V3 xv yv zv)
+move :: (Int, Int) -> (Int, Int)
+move (c, v) = (c + v, v)
 
-energy :: (V3, V3) -> Int
-energy (V3 x y z, V3 xv yv zv) =
-  (abs x + abs y + abs z) * (abs xv + abs yv + abs zv)
+energy :: U -> Int
+energy l = sum (map f (transpose l))
+  where f l = let (c, v) = unzip l in sum (map abs c) * sum (map abs v)
 
-findCycle :: Ord a => [a] -> Int
-findCycle l = f l S.empty
+findCycles :: [U] -> [Int]
+findCycles (  []      : _) = []
+findCycles l@((_ : _) : _) = f (map head l) S.empty : findCycles (map tail l)
   where f (x : xs) s = if S.member x s then S.size s else f xs (S.insert x s)
 
 main :: IO ()
 main = do
-  vs <- parse <$> getContents
-  let steps = iterate sim (zip vs (repeat (V3 0 0 0)))
-  print $ sum $ map energy (steps !! 1000)
-  let c f = findCycle $ map (map (\(c, v) -> (f c, f v))) steps
-  print $ foldl1 lcm [c _x, c _y, c _z]
+  (x, y, z) <- unzip3 . parse <$> getContents
+  let steps = iterate (map sim) (map (map (, 0)) [x, y, z])
+  print $ energy (steps !! 1000)
+  print $ foldl1 lcm (findCycles steps)
